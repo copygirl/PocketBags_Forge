047f6c5fde2c4120d83f62459200b5f783b65c1f
copygirl <halfnerd.copygirl@gmail.com>
2014-09-17 12:35:26 +0200

    Prettify Thaumic Tinkerer registration a little

--- src/net/mcft/copy/bags/addon/ThaumicTinkererAddon.java
+++ src/net/mcft/copy/bags/addon/ThaumicTinkererAddon.java
@@ -5,6 +5,8 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ThaumicTinkererAddon extends Addon {
 	
+	private static final String[] types = { "aer", "ignis", "terra", "aqua" };
+	
 	public static final ThaumicTinkererAddon instance = new ThaumicTinkererAddon();
 	
 	private ThaumicTinkererAddon() { super("ThaumicTinkerer"); }
@@ -15,15 +17,10 @@ public class ThaumicTinkererAddon extends Addon {
 		Item infusedSeed = GameRegistry.findItem("ThaumicTinkerer", "infusedSeeds");
 		Item infusedGrain = GameRegistry.findItem("ThaumicTinkerer", "infusedGrain");
 		
-		registerPouchItem(infusedSeed, 0, "aer_seeds", true, false);
-		registerPouchItem(infusedSeed, 1, "ignis_seeds", true, false);
-		registerPouchItem(infusedSeed, 2, "terra_seeds", true, false);
-		registerPouchItem(infusedSeed, 3, "aqua_seeds", true, false);
-		
-		registerPouchItem(infusedGrain, 0, "aer_grain", true, false);
-		registerPouchItem(infusedGrain, 1, "ignis_grain", true, false);
-		registerPouchItem(infusedGrain, 2, "terra_grain", true, false);
-		registerPouchItem(infusedGrain, 3, "aqua_grain", true, false);
+		for (int i = 0; i < types.length; i++) {
+			registerPouchItem(infusedSeed, i, types[i] + "_seeds", true, false);
+			registerPouchItem(infusedGrain, i, types[i] + "_grain", false, false);
+		}
 		
 	}
 	
